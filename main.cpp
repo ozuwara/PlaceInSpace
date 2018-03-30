12afb61058f0b88d784bacc521549c48630f9ed7
mdg680 <monoper@gmail.com>
2018-03-30 18:26:37 +0200

    Initial SDL window up and running ^_^

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <stdio.h>
-#include "src/gfx/mainFrame.h"
+//#include "src/gfx/mainFrame.h"
 #include <SDL.h>
 
-int main(){
-    printf("123");
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-    int x = sdlEntry();
+//#define WinMain main
+// int argc, char** argv
+int main(int argc, char** argv){
+
+    printf("running...");
+
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf( "SDL could not initialise! SDL_error %s\n : ", SDL_GetError() );
+    }
+    else{
+        window = SDL_CreateWindow("PlaceInSpace", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if(window == NULL){
+            printf("window could not be created! Error: %s\n", SDL_GetError());
+        }
+        else{
+            
+            screenSurface = SDL_GetWindowSurface(window);
+
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            SDL_UpdateWindowSurface(window);
+
+            SDL_Delay(2000);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
-OBJS = main.cpp src/gfx/mainFrame.cpp
+OBJS = main.cpp #src/gfx/mainFrame.cpp
+
+CC = g++
 
 INCLUDE_PATHS = -IC:\Users\monop\Source\Repos\PlaceInSpace\PlaceInSpace\include\SDL2
 
@@ -8,7 +10,11 @@ COMPILER_FLAGS = -w -Wl,-subsystem,windows
 
 LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2
 
-OBJS_NAME = main2.exe
+OBJS_NAME = main.exe
 
-all:
-	g++ $(OBJS) $(INCLUDE_PATHS) $(LIRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_PATHS) -o $(OBJS_NAME)
+all : $(OBJS)
+	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJS_NAME)
+
+#all:
+#	gcc -c $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o main.o
+#	gcc main.o -o $(OBJS_NAME) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS)

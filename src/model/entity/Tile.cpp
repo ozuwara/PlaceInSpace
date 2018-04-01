906b4cd73f3d6e08b98212ec2c7861ff5b4494cc
mdg680 <monoper@gmail.com>
2018-04-01 13:27:24 +0200

    Fixed typos in Tile.cpp
    Added getters and setters in Tile.cpp

--- src/model/entity/Tile.cpp
+++ src/model/entity/Tile.cpp
@@ -7,20 +7,48 @@ class Tile{
     bool traversible;
 
     public:
+        Tile(int init_position_x, int init_position_y);
+
+        int get_position_x();
+        int get_position_y();
         void set_position_x(int);
         void set_position_y(int);
 
+        bool get_traversible();
         void set_traversible(bool);
 };
 
-void tile::set_position_x(int new_position_x){
+Tile::Tile(int init_position_x, int init_position_y){
+    position_x = init_position_x;
+    position_y = init_position_y;
+}
+
+Tile::Tile(int init_position_x, int init_position_y, bool is_traversible){
+    position_x = init_position_x;
+    position_y = init_position_y;
+    bool traversible = is_traversible;
+}
+
+int Tile::get_position_x(){
+    return get_position_x;
+}
+
+int Tile::get_position_y(){
+    return get_position_y;
+}
+
+void Tile::set_position_x(int new_position_x){
     position_x = new_position_x;
 }
 
-void tile::set_position_y(int new_position_y){
+void Tile::set_position_y(int new_position_y){
     position_y = new_position_y;
 }
 
-void tile::set_traversible(bool istraversible){
+bool Tile::get_traversible(){
+    return traversible;
+}
+
+void Tile::set_traversible(bool istraversible){
     traversible = istraversible;
 }  
